a469e5a45fb7afc0f37ea6434a7d38fbc773a798
rxtthin <rotthin_dev@protonmail.com>
2022-11-19 06:20:30 +0100

    Fixed playing incorrect sound on tie

--- src/audio.ts
+++ src/audio.ts
@@ -2,7 +2,7 @@ const winAudio: HTMLAudioElement = new Audio('res/win.wav')
 const tieAudio: HTMLAudioElement = new Audio('res/tie.wav')
 
 export const playWinAudio = () => winAudio.play()
-export const playTieAudio = () => winAudio.play()
+export const playTieAudio = () => tieAudio.play()
 
 export function playClickAudio() {
     const audio: HTMLAudioElement = new Audio('res/pop.wav')
