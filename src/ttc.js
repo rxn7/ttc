a85d643935331f86222fef46858095da1df19f70
rxtthin <rotthin_dev@protonmail.com>
2022-11-25 20:40:12 +0100

    Updates

--- src/ttc.js
+++ src/ttc.js
@@ -1,8 +1,10 @@
 import { playClickAudio, playTieAudio, playWinAudio } from './audio.js';
-const turnText = document.getElementById('turnText');
+const statusText = document.getElementById('statusText');
 const winCountText = document.getElementById('winCountText');
+const winLine = document.getElementById('winLine');
 const PLAYER_O_COLOR = `#faf`;
 const PLAYER_X_COLOR = `#0fa`;
+const TIE_COLOR = `#ffa`;
 const combos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,19 +15,22 @@ const combos = [
     [0, 4, 8],
     [2, 4, 6],
 ];
+const winLineTypes = ['R1', 'R2', 'R3', 'C1', 'C2', 'C3', 'D1', 'D2'];
 let turn = 'O';
 let cells;
 let board = new Array(9);
-let btnClickCounter = 0;
+let cellsFilled = 0;
 let xWinCount = 0;
 let oWinCount = 0;
 let tieCount = 0;
 let inputBlocked = false;
 function initBoard() {
+    inputBlocked = false;
+    cellsFilled = 0;
     updateTurnText();
     updateWinCountText();
-    inputBlocked = false;
-    btnClickCounter = 0;
+    updateCssTurnVariable();
+    winLine.removeAttribute('type');
     if (!cells) {
         cells = Array.from(document.querySelectorAll('#boardContainer td'));
         cells.forEach((cell, i) => {
@@ -44,22 +49,37 @@ function getPlayerColor(player) {
     return PLAYER_X_COLOR;
 }
 function updateTurnText() {
-    turnText.innerHTML = `Turn: <span style="font-weight: bold; color: ${getPlayerColor(turn)}">${turn}</span>`;
+    statusText.innerHTML = `Turn: <span style="font-weight: bold; color: ${getPlayerColor(turn)}">${turn}</span>`;
 }
 function updateWinCountText() {
     winCountText.innerHTML = `<span style="font-weight: bold; color: ${PLAYER_O_COLOR}">O</span> wins: ${oWinCount}<br>`;
     winCountText.innerHTML += `<span style="font-weight: bold; color: ${PLAYER_X_COLOR}">X</span> wins: ${xWinCount}<br>`;
-    winCountText.innerHTML += `ties: ${tieCount}`;
+    winCountText.innerHTML += `<span style="font-weight: bold; color: ${TIE_COLOR}">/</span> ties: ${tieCount}`;
 }
 function flipTurn() {
     turn = turn == 'O' ? 'X' : 'O';
     updateTurnText();
 }
+function updateCssTurnVariable() {
+    if (inputBlocked) {
+        document.documentElement.style.setProperty('--turn', '');
+    }
+    else {
+        document.documentElement.style.setProperty('--turn', JSON.stringify(turn));
+    }
+}
+function setWinLineType(idx) {
+    winLine.setAttribute('type', winLineTypes[idx]);
+}
 function checkWinner() {
+    let i = 0;
     for (let combo of combos) {
         let [a, b, c] = combo;
-        if (board[a] !== false && board[a] === board[b] && board[b] === board[c])
+        if (board[a] !== false && board[a] === board[b] && board[b] === board[c]) {
+            setWinLineType(i);
             return board[a];
+        }
+        ++i;
     }
     return false;
 }
@@ -73,8 +93,13 @@ function onCellClick(cell, idx) {
     const winner = checkWinner();
     if (winner)
         onWin(winner);
-    else if (++btnClickCounter == 9)
+    else if (++cellsFilled == 9)
         onTie();
+    updateCssTurnVariable();
+}
+function scheduleRestart() {
+    inputBlocked = true;
+    setTimeout(initBoard, 3000);
 }
 function onWin(winner) {
     if (winner === 'X')
@@ -82,18 +107,16 @@ function onWin(winner) {
     else
         oWinCount++;
     updateWinCountText();
-    turnText.innerHTML = `<span style="font-weight: bold; color: ${getPlayerColor(winner)}">${winner}</span> wins!`;
-    inputBlocked = true;
     playWinAudio();
-    setTimeout(initBoard, 3000);
+    scheduleRestart();
+    statusText.innerHTML = `<span style="font-weight: bold; color: ${getPlayerColor(winner)}">${winner}</span> wins!`;
 }
 function onTie() {
     tieCount++;
     updateWinCountText();
-    turnText.textContent = `It's a tie!`;
-    inputBlocked = true;
     playTieAudio();
-    setTimeout(initBoard, 3000);
+    scheduleRestart();
+    statusText.innerHTML = `It's a <span style="font-weight: bold; color: ${TIE_COLOR}">/ tie</span>!`;
 }
 initBoard();
 document.documentElement.style.setProperty('--xPlayerColor', PLAYER_X_COLOR);
